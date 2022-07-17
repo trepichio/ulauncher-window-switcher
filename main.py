e0b50e00aa770fd432ce6180a9fac899af792dcf
João Trepichio <joao.trepichio@somoseducacao.com.br>
2022-07-17 03:09:53 -0300

    Add search for open windows and list their title

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-import logging
-from time import sleep
+import subprocess
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
@@ -9,7 +7,7 @@ from ulauncher.api.shared.action.RenderResultListAction import RenderResultListA
 from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
-logger = logging.getLogger(__name__)
+import os
 
 class ZLikeWindowSwitcherExtension(Extension):
     def __init__(self):
@@ -21,18 +19,37 @@ class ZLikeWindowSwitcherExtension(Extension):
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
+        MIN_LEN_TO_SEARCH = 2
         items = []
-        logger.info('preferences %s' % json.dumps(extension.preferences))
-        for i in range(5):
-            item_name = extension.preferences['item_name']
-            data = {'new_name': '%s %s was clicked' % (item_name, i)}
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='%s %s' % (item_name, i),
-                                             description='Item description %s' % i,
-                                             on_enter=ExtensionCustomAction(data, keep_app_open=True)))
+        pid_list = []
+
+        text = event.get_argument() or ''
+
+        if len(text) >= MIN_LEN_TO_SEARCH:
+            pids =  subprocess.Popen(f'xdotool search --onlyvisible "{text}"',
+                shell=True,
+                stdout=subprocess.PIPE
+            ).stdout.read().decode()
+
+            pid_list = pids.splitlines()
+
+            for pid in pid_list:
+                if pid:
+                    windowsname = subprocess.Popen(
+                        'xdotool getwindowname ' + pid,
+                        shell=True,
+                        stdout=subprocess.PIPE
+                    ).stdout.read().decode()
+                    icon_path = 'images/icon.png'
+
+                    items.append(ExtensionResultItem(
+                        icon=icon_path,
+                        name='Open ' + windowsname,
+                        on_enter=ExtensionCustomAction(pid, keep_app_open=True)
+                    ))
+            return RenderResultListAction(items)
 
         return RenderResultListAction(items)
-
 
 class ItemEnterEventListener(EventListener):
 
